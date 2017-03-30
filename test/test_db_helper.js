217af90d5843833f01d61404236ed36aba2be39d
Irene Canuti <irene.canuti@gmail.com>
2017-03-30 09:59:42 +0100

    WIP

--- test/test_db_helper.js
+++ test/test_db_helper.js
@@ -101,59 +101,45 @@ describe('DBhelper', function(){
         }
       ];
 
-    it ('should return the ongoing event', function(done) {
-
-      // var thenable = { then: function(resolve) {
-      //   resolve(items);
-      // }};
+    it ('should return the ongoing event', function() {
 
       var fakeBookedEventsTable = function() {
         return {
           findAll: function() {
-            return Promise.resolve({
-              then: function(onFulfill, onReject) { onFulfill(items)}
+            return new Promise(function(resolve){
+              resolve(items);
             });
           }
         }
       }
 
       var subject = new DBhelper(fakeBookedEventsTable);
+      console.log("promise1", subject.readRoomDateRecordsForTime("big room 2017-03-29", "2017-03-29", "14:00"));
+      // the promise is pending //
 
-      console.log("HERE", subject);
-      console.log("HERE3", fakeBookedEventsTable);
+      return expect(subject.readRoomDateRecordsForTime("big room 2017-03-29", "2017-03-29", "14:00")).to.eventually.equal({
+                "RoomDate": "big room 2017-03-29",
+                "RoomName": "big room",
+                "Owner": "stefan",
+                "Name": "snore meditation",
+                "Date": "2017-03-29",
+                "StartTime": "14:00",
+                "Duration": "PT30M"
+              });
 
 
-
-    //   subject.readRoomDateRecords("joy room 2017-03-27").then(function(candidate) {
-    //     expect(candidate).to.equal(record);
-    //     done();
-    //   });
-    // });
-
-      subject.readRoomDateRecordsForTime("big room 2017-03-29", "2017-03-29", "14:00").then(function(candidate) {
-           console.log("callback", candidate);
-           console.log("callback2", candidate);
-          //  var ongoingEvent = records.find(function(record){
-          //    var searchTime = moment(new Date(date2).toISOString().slice(0,10) + " " + time);
-          //    var start = moment(new Date(record.Date).toISOString().slice(0,10) + " " + record.StartTime);
-          //    var duration = moment.duration(record.Duration, moment.ISO_8601).asMinutes();
-          //    var end = start.clone().add(duration, 'minutes');
-          //    if (start <= searchTime && searchTime <= end) {
-          //      return record;
-          //    }
-          //  });
-          //  return ongoingEvent;
-           expect(candidate).to.equal({
-                     "RoomDate": "big room 2017-03-29",
-                     "RoomName": "big room",
-                     "Owner": "stefan",
-                     "Name": "snore meditation",
-                     "Date": "2017-03-29",
-                     "StartTime": "14:00",
-                     "Duration": "PT30M"
-                   });
-        done();
-        });
+      // .then(function(candidate) {
+      //      console.log("callback", candidate);
+      //      expect(candidate).to.eventually.equal({
+      //                "RoomDate": "big room 2017-03-29",
+      //                "RoomName": "big room",
+      //                "Owner": "stefan",
+      //                "Name": "snore meditation",
+      //                "Date": "2017-03-29",
+      //                "StartTime": "14:00",
+      //                "Duration": "PT30M"
+      //              });
+      // });
     });
 
   });
